0fca274b140f4e003635030fabdfdb82fada5731
solicitosoftware <solicito.software@gmail.com>
2024-03-16 20:11:13 -0500

    test container

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -15,7 +15,7 @@ export default async function DashboardLayout({ children }: Props) {
   }
 
   return (
-    <div id="container" className="flex">
+    <div id="container" className="flex w-full h-full">
       <Sidebar user={user} />
       <div className="w-full">{children}</div>
     </div>
